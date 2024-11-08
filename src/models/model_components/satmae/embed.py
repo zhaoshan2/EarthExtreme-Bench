66e2772f596886fb6f293e45377807a5981dac4e
Shan Zhao <zhaoshanuestc@gmail.com>
2024-11-08 10:30:25 +0000

    Add Dofa model

---
+++ src/models/model_components/satmae/embed.py
@@ -0,0 +1,119 @@
+import numpy as np
+import torch.nn as nn
+from timm.models.layers import to_2tuple
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float32)
+    omega /= embed_dim / 2.0
+    omega = 1.0 / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum("m,d->md", pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out)  # (M, D/2)
+    emb_cos = np.cos(out)  # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb
+
+
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 2 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = np.concatenate([emb_h, emb_w], axis=1)  # (H*W, D)
+    return emb
+
+
+def get_3d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
+    """
+    grid_size: 3d tuple of grid size: t, h, w
+    return:
+    pos_embed: L, D
+    """
+
+    assert embed_dim % 16 == 0
+
+    t_size, h_size, w_size = grid_size
+
+    w_embed_dim = embed_dim // 16 * 6
+    h_embed_dim = embed_dim // 16 * 6
+    t_embed_dim = embed_dim // 16 * 4
+
+    w_pos_embed = get_1d_sincos_pos_embed_from_grid(w_embed_dim, np.arange(w_size))
+    h_pos_embed = get_1d_sincos_pos_embed_from_grid(h_embed_dim, np.arange(h_size))
+    t_pos_embed = get_1d_sincos_pos_embed_from_grid(t_embed_dim, np.arange(t_size))
+
+    w_pos_embed = np.tile(w_pos_embed, (t_size * h_size, 1))
+    h_pos_embed = np.tile(np.repeat(h_pos_embed, w_size, axis=0), (t_size, 1))
+    t_pos_embed = np.repeat(t_pos_embed, h_size * w_size, axis=0)
+
+    pos_embed = np.concatenate((w_pos_embed, h_pos_embed, t_pos_embed), axis=1)
+
+    if cls_token:
+        pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
+    return pos_embed
+
+
+class PatchEmbed(nn.Module):
+    """Frames of 2D Images to Patch Embedding
+    The 3D version of timm.models.vision_transformer.PatchEmbed
+    """
+
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=16,
+        num_frames=3,
+        tubelet_size=1,
+        in_chans=3,
+        embed_dim=768,
+        norm_layer=None,
+        flatten=True,
+        bias=True,
+    ):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_frames = num_frames
+        self.tubelet_size = tubelet_size
+        self.grid_size = (
+            num_frames // tubelet_size,
+            img_size[0] // patch_size[0],
+            img_size[1] // patch_size[1],
+        )
+        self.num_patches = self.grid_size[0] * self.grid_size[1] * self.grid_size[2]
+        self.flatten = flatten
+
+        self.proj = nn.Conv3d(
+            in_chans,
+            embed_dim,
+            kernel_size=(tubelet_size, patch_size[0], patch_size[1]),
+            stride=(tubelet_size, patch_size[0], patch_size[1]),
+            bias=bias,
+        )
+        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+
+    def forward(self, x):
+        B, C, T, H, W = x.shape
+        x = self.proj(x)
+        if self.flatten:
+            x = x.flatten(2).transpose(1, 2)  # B,C,T,H,W -> B,C,L -> B,L,C
+        x = self.norm(x)
+        return x
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/models/model_components/satmae/encoder.py
@@ -0,0 +1,100 @@
+import torch.nn as nn
+from training_utils import get_activation, get_normalization, SE_Block
+
+
+class CoreCNNBlock(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        *,
+        norm="batch",
+        activation="relu",
+        padding="same",
+        residual=True,
+    ):
+        super(CoreCNNBlock, self).__init__()
+        self.activation = get_activation(activation)
+        self.residual = residual
+        self.padding = padding
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.squeeze = SE_Block(self.out_channels)
+        self.match_channels = nn.Identity()
+        if in_channels != out_channels:
+            self.match_channels = nn.Sequential(
+                nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, padding=0, bias=False
+                ),
+                get_normalization(norm, out_channels),
+            )
+        self.conv1 = nn.Conv2d(self.in_channels, self.out_channels, 1, padding=0)
+        self.norm1 = get_normalization(norm, self.out_channels)
+        self.conv2 = nn.Conv2d(
+            self.out_channels,
+            self.out_channels,
+            3,
+            padding=self.padding,
+            groups=self.out_channels,
+        )
+        self.norm2 = get_normalization(norm, self.out_channels)
+
+        self.conv3 = nn.Conv2d(
+            self.out_channels, self.out_channels, 3, padding=self.padding, groups=1
+        )
+        self.norm3 = get_normalization(norm, self.out_channels)
+
+    def forward(self, x):
+        identity = x
+        x = self.activation(self.norm1(self.conv1(x)))
+        x = self.activation(self.norm2(self.conv2(x)))
+        x = self.norm3(self.conv3(x))
+        x = x * self.squeeze(x)
+        if self.residual:
+            x = x + self.match_channels(identity)
+        x = self.activation(x)
+        return x
+
+
+class EncoderBlock(nn.Module):
+    def __init__(
+        self,
+        depth,
+        in_channels,
+        out_channels,
+        norm="batch",
+        activation="relu",
+        padding="same",
+    ):
+        super(EncoderBlock, self).__init__()
+
+        self.depth = depth
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.activation = activation
+        self.norm = norm
+        self.padding = padding
+
+        self.blocks = []
+        for i in range(self.depth):
+            _in_channels = self.in_channels if i == 0 else self.out_channels
+            block = CoreCNNBlock(
+                _in_channels,
+                self.out_channels,
+                norm=self.norm,
+                activation=self.activation,
+                padding=self.padding,
+            )
+
+            self.blocks.append(block)
+
+        self.blocks = nn.Sequential(*self.blocks)
+        self.downsample = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        for i in range(self.depth):
+            x = self.blocks[i](x)
+
+        x = self.downsample(x)
+
+        return x
